94610e53cda51b3581872779028700b5890c4698
Mustafa Elnaquib <melnaquib@gmail.com>
2021-05-05 04:31:59 -0700

    add color to robot body

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,4 +8,3 @@
 
 # modified environment variables
 export PWD='/home/robond/workspace/catkin_ws/build'
-export PYTHONPATH="/home/robond/workspace/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
